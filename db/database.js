8d35d4dcdef8ea33b725a6ad6de458a1ecc53141
Matt Latham <latham75@gmail.com>
2016-09-21 16:42:01 -0500

    data change

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose')
 
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/userlogintest'
+const MONGODB_URL = process.env.MONGODB_URL || "localhost:5757"
 
 mongoose.Promise = Promise
 
